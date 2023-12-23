03bde8a2d2b37ff6bdee5029cf11e995eb9b14d2
jake <palatable7@naver.com>
2023-12-23 22:18:10 +0900

    Implement

--- ExImageOptimazationBackground/ViewController.swift
+++ ExImageOptimazationBackground/ViewController.swift
@@ -8,52 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private let imageView: UIImageView = {
-        let view = UIImageView()
-//        view.image = UIImage(named: "img") /// <- 무작정 이미지를 대입시키지 않고 이미지가 보일때만 image를 사용하기
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
+    private let scrollView = {
+        let v = UIScrollView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
     }()
-
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bind()
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
         
-        view.addSubview(imageView)
         NSLayoutConstraint.activate([
-            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
         ])
-    }
-    
-    func bind() {
-        NotificationCenter.default.addObserver(
-            forName: UIApplication.willEnterForegroundNotification,
-            object: nil,
-            queue: .main
-        ) { [weak self] (note) in
-            print("foreground")
-            self?.loadImage()
-        }
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+        ])
         
-        NotificationCenter.default.addObserver(
-            forName: UIApplication.didEnterBackgroundNotification,
-            object: nil,
-            queue: .main
-        ) { [weak self] (note) in
-            print("background")
-            self?.unloadImage()
+        addImages()
+    }
+    
+    func addImages() {
+        let image = UIImage(named: "high_resolution_img")
+        
+        (0...10000)
+            .forEach { _ in
+                let imageView = UIImageView(image: resizeImage(image: image!, targetSize: .init(width: 300, height: 300)))
+                stackView.addArrangedSubview(imageView)
+            }
+    }
+    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+
+        var newSize: CGSize
+        if widthRatio > heightRatio {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
         }
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: CGRect(origin: .zero, size: newSize))
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage ?? image
     }
-    
-    func loadImage() {
-        imageView.image = UIImage(named: "high_resolution_img")
-    }
-    
-    func unloadImage() {
-        imageView.image = nil
-    }
+
 }
